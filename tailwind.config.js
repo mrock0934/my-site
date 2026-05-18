175b85694b62d8a848c21be041433d1d8ba845a0
mrock0934 <mrock0934@gmail.com>
2026-05-17 21:39:09 -0500

    Add typography plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,15 @@
 // tailwind.config.js
+import typography from "@tailwindcss/typography";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     fontFamily: {
-      sans: ['"Inter"', "sans-serif"],      // body & nav
-      serif: ['"EB Garamond"', "serif"],   // headings
-      mono: ['"Fira Mono"', "monospace"],  // meta/captions if you want
+      sans: ['"Inter"', "sans-serif"],
+      serif: ['"EB Garamond"', "serif"],
+      mono: ['"Fira Mono"', "monospace"],
     },
     extend: {},
   },
-  plugins: [],
+  plugins: [typography],
 };
